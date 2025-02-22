20cd6e7efcf60b7291157bea8e29bcb9b01a6607
Mateusz Czapliński <czapkofan@gmail.com>
2025-02-22 21:30:07 +0100

    build: tweak hex.bat for better usability

--- hex.bat
+++ hex.bat
@@ -2,3 +2,4 @@
 set P=C:\Users\Mateusz\AppData\Local\Arduino15\packages\teensy\tools\teensy-compile\11.3.1\avr\bin
 set F=target\avr-atmega32u4\debug\chordite-rust
 %P%\avr-objcopy -O ihex -R .eeprom -R .fuse -R .lock -R .signature %F%.elf %F%.hex
+dir %F%.*
