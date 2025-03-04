97dbca50ccf07c6c5baeba1b19c29aa03bde3fee
Mateusz Czapliński <czapkofan@gmail.com>
2025-03-04 22:15:18 +0100

    build: gen hex also for release builds

--- hex.bat
+++ hex.bat
@@ -1,5 +1,10 @@
 @echo off
 set P=C:\Users\Mateusz\AppData\Local\Arduino15\packages\teensy\tools\teensy-compile\11.3.1\avr\bin
+
 set F=target\avr-atmega32u4\debug\chordite-rust
+%P%\avr-objcopy -O ihex -R .eeprom -R .fuse -R .lock -R .signature %F%.elf %F%.hex
+dir %F%.*
+
+set F=target\avr-atmega32u4\release\chordite-rust
 %P%\avr-objcopy -O ihex -R .eeprom -R .fuse -R .lock -R .signature %F%.elf %F%.hex
 dir %F%.*
