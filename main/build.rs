ecb9c41fb3b8c56f0cba13d21db50b55af404a79
Mateusz Czapliński <czapkofan@gmail.com>
2025-03-26 19:29:39 +0100

    drop defmt & other feats + switch to panic_halt
    Goal is to reduce size

--- main/build.rs
+++ main/build.rs
@@ -31,6 +31,6 @@ fn main() {
 
     println!("cargo:rustc-link-arg-bins=--nmagic");
     println!("cargo:rustc-link-arg-bins=-Tlink.x");
-    println!("cargo:rustc-link-arg-bins=-Tdefmt.x");
+    // println!("cargo:rustc-link-arg-bins=-Tdefmt.x");
 }
 
--- main/src/main.rs
+++ main/src/main.rs
@@ -20,14 +20,12 @@
 use embassy_executor::Spawner;
 use embassy_nrf::gpio::{Level, Output, OutputDrive};
 use embassy_time::Timer;
-use {defmt_rtt as _, panic_probe as _};
+use panic_halt as _;
 
 #[embassy_executor::main]
 async fn main(_spawner: Spawner) {
     let p = embassy_nrf::init(Default::default());
     let mut led = Output::new(p.P0_21, Level::Low, OutputDrive::Standard);
-
-    // defmt::println!("Hello nrf!");
 
     loop {
         led.set_high();
